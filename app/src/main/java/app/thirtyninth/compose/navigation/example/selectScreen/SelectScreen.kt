0b3f9ede9c321944095c18e658f49dd1a6d1b287
Konstantin Kolchurin <kkolchurin@icerockdev.com>
2023-10-31 01:26:52 +0700

    remove surface

--- app/src/main/java/app/thirtyninth/compose/navigation/example/selectScreen/SelectScreen.kt
+++ app/src/main/java/app/thirtyninth/compose/navigation/example/selectScreen/SelectScreen.kt
@@ -10,7 +10,6 @@ import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.height
 import androidx.compose.material3.Button
 import androidx.compose.material3.OutlinedTextField
-import androidx.compose.material3.Surface
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.MutableState
@@ -38,131 +37,129 @@ object SelectScreen : Screen {
     override fun Content(navController: NavController, args: Bundle?) {
         val context = LocalContext.current
 
-        Surface {
-            Box(modifier = Modifier.fillMaxSize()) {
-                Column(
+        Box(modifier = Modifier.fillMaxSize()) {
+            Column(
+                modifier = Modifier
+                    .align(Alignment.Center)
+                    .fillMaxWidth(0.8f),
+                horizontalAlignment = Alignment.CenterHorizontally,
+                verticalArrangement = Arrangement.spacedBy(16.dp)
+            ) {
+                val requiredValue: MutableState<String> = remember {
+                    mutableStateOf("Required")
+                }
+                val optionalValue: MutableState<String> = remember {
+                    mutableStateOf("Optional 1")
+                }
+
+                Text(text = stringResource(id = R.string.field_required_param))
+                OutlinedTextField(
+                    value = requiredValue.value,
+                    onValueChange = { newText ->
+                        requiredValue.value = newText
+                    },
+                    maxLines = 1
+                )
+
+                Text(text = stringResource(id = R.string.field_optional_param1))
+                OutlinedTextField(
+                    value = optionalValue.value,
+                    onValueChange = { newText ->
+                        optionalValue.value = newText
+                    },
+                    maxLines = 1
+                )
+
+                Button(
                     modifier = Modifier
-                        .align(Alignment.Center)
-                        .fillMaxWidth(0.8f),
-                    horizontalAlignment = Alignment.CenterHorizontally,
-                    verticalArrangement = Arrangement.spacedBy(16.dp)
+                        .height(80.dp)
+                        .fillMaxWidth(),
+                    onClick = {
+                        navController.navigate(
+                            DefaultScreen.screenName
+                        )
+                    }
                 ) {
-                    val requiredValue: MutableState<String> = remember {
-                        mutableStateOf("Required")
-                    }
-                    val optionalValue: MutableState<String> = remember {
-                        mutableStateOf("Optional 1")
-                    }
-
-                    Text(text = stringResource(id = R.string.field_required_param))
-                    OutlinedTextField(
-                        value = requiredValue.value,
-                        onValueChange = { newText ->
-                            requiredValue.value = newText
-                        },
-                        maxLines = 1
+                    Text(
+                        text = stringResource(id = R.string.navigate_to_default),
+                        textAlign = TextAlign.Center
                     )
-
-                    Text(text = stringResource(id = R.string.field_optional_param1))
-                    OutlinedTextField(
-                        value = optionalValue.value,
-                        onValueChange = { newText ->
-                            optionalValue.value = newText
-                        },
-                        maxLines = 1
-                    )
-
-                    Button(
-                        modifier = Modifier
-                            .height(80.dp)
-                            .fillMaxWidth(),
-                        onClick = {
-                            navController.navigate(
-                                DefaultScreen.screenName
+                }
+                Button(
+                    modifier = Modifier
+                        .height(80.dp)
+                        .fillMaxWidth(),
+                    onClick = {
+                        navController.navigate(
+                            ScreenWithRequiredParams.screenName(
+                                argument = requiredValue.value
                             )
-                        }
-                    ) {
-                        Text(
-                            text = stringResource(id = R.string.navigate_to_default),
-                            textAlign = TextAlign.Center
                         )
                     }
-                    Button(
-                        modifier = Modifier
-                            .height(80.dp)
-                            .fillMaxWidth(),
-                        onClick = {
-                            navController.navigate(
-                                ScreenWithRequiredParams.screenName(
-                                    argument = requiredValue.value
+                ) {
+                    Text(
+                        text = stringResource(id = R.string.navigate_to_screen_with_required_params),
+                        textAlign = TextAlign.Center
+                    )
+                }
+                Button(
+                    modifier = Modifier
+                        .height(80.dp)
+                        .fillMaxWidth(),
+                    onClick = {
+                        val inputFieldValue: String? = optionalValue.value.ifEmpty { null }
+
+                        navController.navigate(
+                            when (inputFieldValue == null) {
+                                true -> ScreenWithOptionalParams.screenNameWithoutArgument()
+                                false -> ScreenWithOptionalParams.screenNameWithArgument(
+                                    argument = inputFieldValue
                                 )
-                            )
-                        }
-                    ) {
-                        Text(
-                            text = stringResource(id = R.string.navigate_to_screen_with_required_params),
-                            textAlign = TextAlign.Center
-                        )
-                    }
-                    Button(
-                        modifier = Modifier
-                            .height(80.dp)
-                            .fillMaxWidth(),
-                        onClick = {
-                            val inputFieldValue: String? = optionalValue.value.ifEmpty { null }
-
-                            navController.navigate(
-                                when (inputFieldValue == null) {
-                                    true -> ScreenWithOptionalParams.screenNameWithoutArgument()
-                                    false -> ScreenWithOptionalParams.screenNameWithArgument(
-                                        argument = inputFieldValue
-                                    )
-                                }
-                            )
-                        }
-                    ) {
-                        Text(
-                            text = stringResource(id = R.string.navigate_to_screen_with_optional_params),
-                            textAlign = TextAlign.Center
-                        )
-                    }
-
-                    Button(
-                        modifier = Modifier
-                            .height(80.dp)
-                            .fillMaxWidth(),
-                        onClick = {
-                            val requiredFieldValue: String? = requiredValue.value.ifEmpty { null }
-                            val optionalFieldValue: String? = optionalValue.value.ifEmpty { null }
-
-                            if (requiredFieldValue == null){
-                                Toast.makeText(
-                                    context,
-                                    "Please, input required field value",
-                                    Toast.LENGTH_SHORT
-                                ).show()
-
-                                return@Button
                             }
-
-                            navController.navigate(
-                                when (optionalFieldValue == null) {
-                                    true -> ScreenWithParams.screenNameWithRequiredArgument(
-                                        argument = requiredFieldValue
-                                    )
-                                    false -> ScreenWithParams.screenNameWithArguments(
-                                        argument = requiredFieldValue,
-                                        optionalArgument = optionalFieldValue
-                                    )
-                                }
-                            )
-                        }
-                    ) {
-                        Text(
-                            text = stringResource(id = R.string.navigate_to_screen_with_required_and_optional_params),
-                            textAlign = TextAlign.Center
                         )
                     }
+                ) {
+                    Text(
+                        text = stringResource(id = R.string.navigate_to_screen_with_optional_params),
+                        textAlign = TextAlign.Center
+                    )
+                }
+
+                Button(
+                    modifier = Modifier
+                        .height(80.dp)
+                        .fillMaxWidth(),
+                    onClick = {
+                        val requiredFieldValue: String? = requiredValue.value.ifEmpty { null }
+                        val optionalFieldValue: String? = optionalValue.value.ifEmpty { null }
+
+                        if (requiredFieldValue == null){
+                            Toast.makeText(
+                                context,
+                                "Please, input required field value",
+                                Toast.LENGTH_SHORT
+                            ).show()
+
+                            return@Button
+                        }
+
+                        navController.navigate(
+                            when (optionalFieldValue == null) {
+                                true -> ScreenWithParams.screenNameWithRequiredArgument(
+                                    argument = requiredFieldValue
+                                )
+                                false -> ScreenWithParams.screenNameWithArguments(
+                                    argument = requiredFieldValue,
+                                    optionalArgument = optionalFieldValue
+                                )
+                            }
+                        )
+                    }
+                ) {
+                    Text(
+                        text = stringResource(id = R.string.navigate_to_screen_with_required_and_optional_params),
+                        textAlign = TextAlign.Center
+                    )
                 }
             }
         }
